ac7c833093e106f2c5e5b9a61cc51f9675995ccd
Sankara Narayanan Rajagopal <sankaranarayananrajagopal@Sankaras-MacBook-Pro.local>
2023-01-18 15:58:44 -0500

    Initial commit

---
+++ app/src/main/java/com/example/inclass_sankara_narayanan_002787959/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.inclass_sankara_narayanan_002787959;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.Assignment1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent newIntent = new Intent(MainActivity.this,PracticeActivity.class);
+                startActivity(newIntent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/inclass_sankara_narayanan_002787959/PracticeActivity.java
@@ -0,0 +1,40 @@
+package com.example.inclass_sankara_narayanan_002787959;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class PracticeActivity extends AppCompatActivity {
+
+    Button logButton,toastButton;
+
+    final String TAG = "LOGGER";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practice);
+        setTitle("Assignment1");
+
+        logButton = findViewById(R.id.logButton);
+        toastButton =findViewById(R.id.toastButton);
+
+        logButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "Practice!Practice!!Practice!!!");
+            }
+        });
+
+        toastButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(PracticeActivity.this,"Now push to GitHub and Submit!",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
